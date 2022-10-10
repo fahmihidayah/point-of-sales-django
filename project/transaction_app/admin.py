a066d8e49539aeb866e1f971dfa6ac90c0d4b932
fahmi-mac <f.hidayah@pixilapps.com>
2022-10-10 22:09:12 +0700

    add transaction admin

--- project/transaction_app/admin.py
+++ project/transaction_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Transaction
+from .forms import TransactionForm
 
 # Register your models here.
+
+
+@admin.register(Transaction)
+class TransactionModelAdmin(admin.ModelAdmin):
+
+    form = TransactionForm
+    list_display = ['pk', 'invoice_number', 'total', 'user', 'created_at', 'updated_at']
+
--- project/transaction_app/forms.py
+++ project/transaction_app/forms.py
@@ -1,3 +1,8 @@
 from django.forms import ModelForm
 from . import models
 
+class TransactionForm(ModelForm):
+
+    class Meta:
+        model = models.Transaction
+        fields = ['invoice_number', 'total', 'user', 'company']
